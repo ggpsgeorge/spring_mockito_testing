423b5cef4c63cc52ef3f8e1e204f7dcf26e316c8
ggpsgeorge <ggpsgeorge@gmail.com>
2023-11-13 14:42:09 -0300

    repository annotation

--- src/main/java/com/ggpsgeorge/spring_testing_tutorial/UserRepository.java
+++ src/main/java/com/ggpsgeorge/spring_testing_tutorial/UserRepository.java
@@ -1,7 +1,9 @@
 package com.ggpsgeorge.spring_testing_tutorial;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
